90d3e86c7d74688914e5d25934aa9b15d3914da4
=Ta Ngoc <=tangoc98@gmail.com>
2020-05-30 10:18:52 +0700

    new push

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x - 2, transform.position.y, transform.position.z), 5);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, transform.position.z - 2), 5);
+
+        }
+        //if (Input.touchCount == 1)
+        //{
+        //    var touch = Input.touches[0];
+        //    if (touch.position.x < Screen.width / 2)
+        //    {
+        //        transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x - 2, transform.position.y, transform.position.z), 5);
+        //        Debug.Log("left");
+        //    }
+        //    else if (touch.position.x > Screen.width / 2)
+        //    {
+        //        transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, transform.position.z - 2), 5);
+        //        Debug.Log("right");
+        //    }
+        //}
+    }
+}
